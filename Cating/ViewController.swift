451b43b72acdc75ffc80f759c6489c2ffa417fe4
Fauzi <fauzibinfaisal@gmail.com>
2019-08-28 13:34:04 +0700

    Initial Commit

---
+++ Cating/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cating
+//
+//  Created by Fauzi Fauzi on 28/08/19.
+//  Copyright © 2019 fauzify. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
